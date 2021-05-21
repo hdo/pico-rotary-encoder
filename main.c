de452c18b52b84253d74c447728123eda3c93c83
Huy Do <huydo1@gmail.com>
2021-05-21 09:46:31 +0200

    refactor to support multiple encoder

--- main.c
+++ main.c
@@ -8,118 +8,77 @@
 #define GPIO_ENCODER1_B  17  // YELLOW
 
 
+
+typedef struct ROTARY_ENCODER_DATA
+{
+    uint8_t gpio_a, gpio_b;       // required
+    int16_t min_value, max_value; // required
+    uint8_t triggered;  
+    uint32_t wait_until, wait_until_trigger;
+    uint8_t filtered_a, last_a, last_filtered_a;
+    int8_t dir;
+    int16_t current_value;      
+
+} rotary_encoder_t;
+
+
+
+rotary_encoder_t encoder1;
+
+
 const uint8_t LED_PIN = 25;
 
 uint32_t last_led = 0;
 
-uint32_t last_event = 0;
-uint32_t last_pulse = 0;
-
-uint32_t last_pulse_a = 0;
-uint32_t last_pulse_b = 0;
-
-uint32_t last_a_rise = 0;
-uint32_t last_a_fall = 0;
-uint32_t last_b_rise = 0;
-uint32_t last_b_fall = 0;
-
-uint32_t last_event_a, last_event_b;
-
-uint8_t last_a, last_b, a_event, b_event;
-
-uint8_t triggered = 0;
-uint32_t triggered_at = 0;
-uint32_t last_trigger = 0;
-uint32_t wait_until = 0;
-
-int8_t last_dir = 0;
-uint32_t last_dir_change = 0;
-uint32_t last_state_change = 0;
-
-uint8_t filtered_a, filtered_b, last_af, last_bf;
-
-int16_t value = 0;
 
 uint32_t current_tick()  {
     return time_us_32() / 1000;
 }
 
-void rotary_task6() {
+void rotary_task(rotary_encoder_t *data) {
     uint32_t current_us = time_us_32();
 
-    uint8_t a = gpio_get(GPIO_ENCODER1_A);
-    uint8_t b = gpio_get(GPIO_ENCODER1_B);
+    uint8_t a = gpio_get(data->gpio_a);
+    uint8_t b = gpio_get(data->gpio_b);
 
-    if (triggered && current_us > wait_until) {
-        triggered = 0;
-        filtered_a = a;
+    if (data->triggered && current_us > data->wait_until) {
+        data->triggered = 0;
+        data->filtered_a = a;
     }
 
-    if (a != last_a) {
-        triggered = 1;
-        wait_until = current_us + 2000;
+    if (a != data->last_a) {
+        data->triggered = 1;
+        data->wait_until = current_us + 2000;
     }
 
     // state change on filtered A
-    if (filtered_a != last_af) {        
+    if (data->filtered_a != data->last_filtered_a) {        
         // only allow signal everey 5ms
-        if (current_us - last_trigger > 5000) {
-            int8_t dir = 0;
-            if (filtered_a == b) {
+        if (current_us > data->wait_until_trigger) {
+            data->dir = 0;
+            if ((data->filtered_a) == b) {
                 //puts("R");
-                dir = 1;
+                data->dir = 1;
             }
             else {
                 //puts("L");
-                dir = -1;
+                data->dir = -1;
             }
-            value += dir;
-            printf("%d \r\n", value);
+            data->current_value += data->dir;
+            if (data->current_value < data->min_value) {
+                data->current_value = data->min_value;
+            }
+            if (data->current_value > data->max_value) {
+                data->current_value = data->max_value;
+            }
+            printf("%d \r\n", data->current_value);
         }
-        last_trigger = current_us;
+        data->wait_until_trigger = current_us + 5000;
     }
 
-    last_af = filtered_a;    
-    last_a = a;
+    data->last_filtered_a = data->filtered_a;    
+    data->last_a = a;
 }
-
-
-
-void rotary_task5() {
-    uint32_t current_us = time_us_32();
-
-    uint8_t a = gpio_get(GPIO_ENCODER1_A);
-    uint8_t b = gpio_get(GPIO_ENCODER1_B);
-
-    if (triggered && current_us > wait_until) {
-        triggered = 0;
-        filtered_a = a;
-    }
-
-    if (a != last_a) {
-        triggered = 1;
-        wait_until = current_us + 2000;
-    }
-
-    // state change on filtered A
-    if (filtered_a != last_af) {
-        int8_t dir = 0;
-        if (filtered_a == b) {
-            puts("R");
-            dir = 1;
-        }
-        else {
-            puts("L");
-            dir = -1;
-        }
-        value += dir;
-        printf("%d \r\n", value);
-    }
-
-    last_af = filtered_a;    
-    last_a = a;
-}
-
 
 void init_gpio() {
     gpio_init(LED_PIN);
@@ -145,9 +104,17 @@ int main() {
     //gpio_set_irq_enabled_with_callback(GPIO_ENCODER1_A, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     //gpio_set_irq_enabled_with_callback(GPIO_ENCODER1_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
+    encoder1.gpio_a = GPIO_ENCODER1_A;
+    encoder1.gpio_b = GPIO_ENCODER1_B;
+    encoder1.min_value = 0;
+    encoder1.max_value = 100;
+
+
     while (1) {
         //rotary_task5();
-        rotary_task6();
+        //rotary_task(&encoder1);
+
+        rotary_task(&encoder1);
 
         if (current_tick() - last_led > 200) {
             last_led = current_tick();
